927605a01577114e6c17026faac4d2982b8d8578
Aisalkyn Dzhenalieva <aidzhenalieva@gmail.com>
2019-08-17 18:45:19 +0600

    refactored

--- api/fixtures.js
+++ api/fixtures.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
-const nanoid  = require('nanoid');
+const nanoid = require('nanoid');
 const config = require('./config');
 
 
 const User = require('./models/User');
 const Place = require('./models/Place');
+const Gallery = require("./models/Gallery");
 
 
 const run = async () => {
@@ -22,22 +23,66 @@ const run = async () => {
         {username: 'visitor', password: '123', token: nanoid(), displayName: 'Visitor', avatar: 'leo.jpeg'},
         {username: 'leo', password: '123', token: nanoid(), displayName: 'Leo', avatar: 'leo.jpeg'},
         {username: 'test', password: '123', token: nanoid(), displayName: 'Test', avatar: 'leo.jpeg'},
-
     );
 
     const places = await Place.create(
-        {user: users[0], description: "Oriental cuisine, bright interior and polite staff! we work for you", mainImage: 'barashek.jpeg', title: "Barashek", feedback: [
-                { comment: "nice place! delicios food!", user: users[1], rating: {food: 5, service: 5, interior: 5, average: 5}, displayName: "Visitor", datetime: new Date().toISOString()},
-                { comment: "good food, but awful service!", user: users[2], rating: {food: 5, service: 2, interior: 5, average: 4}, displayName: "Leo", datetime: new Date().toISOString()}
-            ]},
-        {user: users[1], description: "European food, design and staff! Best chef from Italy! Ready to meet you 24/7", mainImage: 'adriano.png', title: "Adriano", feedback: [
-                { comment: "delicios and fast!", user: users[2], rating: {food: 5, service: 5, interior: 5, average: 5}, displayName: "Leo", datetime: new Date().toISOString()}
-            ]},
-        {user: users[2], description: "Coffee and pastry! Always fresh and tasty!", mainImage: 'bublick.png', title: "Bublick", feedback: [
-                { comment: "delicios and fast!", user: users[1], rating: {food: 5, service: 5, interior: 5, average: 5}, displayName: "Visitor", datetime: new Date().toISOString()}
-            ]}
+        {
+            user: users[0],
+            description: "Oriental cuisine, bright interior and polite staff! we work for you",
+            mainImage: 'barashek.jpeg',
+            title: "Barashek",
+            feedback: [
+                {
+                    comment: "nice place! delicios food!",
+                    user: users[1],
+                    rating: {food: 5, service: 5, interior: 5, average: 5},
+                    displayName: "Visitor",
+                    datetime: new Date().toISOString()
+                },
+                {
+                    comment: "good food, but awful service!",
+                    user: users[2],
+                    rating: {food: 5, service: 2, interior: 5, average: 4},
+                    displayName: "Leo",
+                    datetime: new Date().toISOString()
+                }
+            ]
+        },
+        {
+            user: users[1],
+            description: "European food, design and staff! Best chef from Italy! Ready to meet you 24/7",
+            mainImage: 'adriano.png',
+            title: "Adriano",
+            feedback: [
+                {
+                    comment: "delicios and fast!",
+                    user: users[2],
+                    rating: {food: 5, service: 5, interior: 5, average: 5},
+                    displayName: "Leo",
+                    datetime: new Date().toISOString()
+                }
+            ]
+        },
+        {
+            user: users[2],
+            description: "Coffee and pastry! Always fresh and tasty!",
+            mainImage: 'bublick.png',
+            title: "Bublick",
+            feedback: [
+                {
+                    comment: "delicios and fast!",
+                    user: users[1],
+                    rating: {food: 5, service: 5, interior: 5, average: 5},
+                    displayName: "Visitor",
+                    datetime: new Date().toISOString()
+                }
+            ]
+        }
     );
 
+    await Gallery.create(
+        {image: ["bublickVisitor.jpeg", "bublickLeo.jpeg"], user: users[1], place: places[2]},
+        {image: ["adrianoCoffee.jpeg", "adrianoLeo.jpeg"], user: users[2], place: places[1]});
 
 
     await connection.close();
